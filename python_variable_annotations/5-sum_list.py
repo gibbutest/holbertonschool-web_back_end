f7a15088961af7121f99a7ebbeb8b2db3e842643
Ian <Gibbu_@hotmail.com>
2025-07-28 15:34:44 +1000

    Task 5 - uhh... the checker needs to be updated.

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 """ The module """
+import typing
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """ Return sum of a list """
     return sum(input_list)
