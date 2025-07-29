4a6242f9afc02cb82e4078f5f67256c7f1de8b0d
Ian <Gibbu_@hotmail.com>
2025-07-29 12:56:28 +1000

    Task 4 - init

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+""" The module """
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> list[float]:
+    """ Spawns tasks n times with max_delay """
+    delays = []
+    tasks = []
+
+    for _ in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
