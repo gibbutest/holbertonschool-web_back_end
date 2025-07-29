843b4c2450dc52872fe2763d0ca276ca7c5b3819
Ian <Gibbu_@hotmail.com>
2025-07-29 13:17:36 +1000

    Task 2 - but... it worked?

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,12 +10,8 @@ async def measure_runtime() -> float:
     """ Measure the total runtime for 4 tasks """
     start_time = time()
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    tasks = [asyncio.create_task(async_comprehension()) for _ in range(4)]
+    await asyncio.gather(*tasks)
 
     end_time = time()
     return end_time - start_time
