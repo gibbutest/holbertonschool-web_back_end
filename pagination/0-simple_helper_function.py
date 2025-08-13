1c3ecef0bd667e10ef1e7c6d9734756f8f604c8b
Ian <Gibbu_@hotmail.com>
2025-08-13 18:33:59 +1000

    Task 0 - comments

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+""" The module """
+
 def index_range(page, page_size):
+    """ Returns a tuple of the starting and end indexes """
+
     start = (page - 1) * page_size
     end = page * page_size
 
