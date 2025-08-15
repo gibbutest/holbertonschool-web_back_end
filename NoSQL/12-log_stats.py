6634f42f678e9eb83aefb2343bc8d59e4371ddfa
Ian <Gibbu_@hotmail.com>
2025-08-15 13:22:08 +1000

    Task 12 - init

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+""" The module """
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb://127.0.0.1:27017")
+collection = client.logs.nginx
+
+total_logs = collection.count_documents({})
+print(f"{total_logs} logs")
+
+print("Methods:")
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for method in methods:
+    count = collection.count_documents({"method": method})
+    print(f"\tmethod {method}: {count}")
+
+status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+print(f"{status_check_count} status check")
