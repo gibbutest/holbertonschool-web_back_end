e7b86552e86e8385ea498d30a2e20264663e10d5
Ian <Gibbu_@hotmail.com>
2025-08-20 16:25:54 +1000

    Task 8 - init

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
-const cleanSet = (set, startString) => {
+export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') return '';
 
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length))
     .join('-');
-};
+}
