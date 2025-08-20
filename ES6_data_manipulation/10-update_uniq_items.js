ad093ebee139b11320deb9015ada740d87496efb
Ian <Gibbu_@hotmail.com>
2025-08-20 16:45:32 +1000

    Task 10 - yeah whoops, forgot entries doesn't return an array

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,9 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) throw new Error('Cannot process');
 
-  map.entries().forEach(([key, value]) => {
-    if (value === 1) map.set(key, 100);
-  });
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
 }
