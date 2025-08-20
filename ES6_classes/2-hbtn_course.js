cbc0d711b741358b876db0b37edd17709bc1ccfb
Ian <Gibbu_@hotmail.com>
2025-08-20 11:25:23 +1000

    Task 2 - constructor type checks

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,9 @@
 export default class HolbertonCourse {
-  constructor(name = '', length = 0, students = []) {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('Students must be an array');
+
     this.name = name;
     this.length = length;
     this.students = students;
