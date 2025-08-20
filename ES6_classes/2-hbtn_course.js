3fb47d8b41c38e6752ff0f33a93f7794ee084956
Ian <Gibbu_@hotmail.com>
2025-08-20 14:38:31 +1000

    Revert back AGAIN

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,16 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students)) {
-      throw new TypeError('Not an array');
-    }
-
-    // Use the setters instead of assigning directly to the priv vars
     this.name = name;
     this.length = length;
     this.students = students;
@@ -19,33 +8,26 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
-
-  set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = newName;
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    this._name = value;
   }
 
   get length() {
     return this._length;
   }
-
-  set length(newLen) {
-    if (typeof newLen !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = Number(newLen);
+  set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    this._length = value;
   }
 
   get students() {
     return this._students;
   }
-
-  set students(newStudents) {
-    if (!Array.isArray(newStudents)) {
-      throw new TypeError('Not an array');
+  set students(value) {
+    if (!Array.isArray(value) || (Array.isArray(value) && value.some((el) => typeof el !== 'string'))) {
+      throw new TypeError('students must be an array of strings');
     }
-    this._students = newStudents;
+    this._students = value;
   }
 }
