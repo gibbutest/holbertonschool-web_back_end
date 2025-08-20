cd6c9d9fdcc12b5219f3cc54b3281f6a55f01dbe
Ian <Gibbu_@hotmail.com>
2025-08-20 11:11:44 +1000

    Task 2 - updated error strings

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -9,7 +9,7 @@ export default class HolbertonCourse {
     return this._name;
   }
   set name(value) {
-    if (typeof value !== 'string') throw Error('name must be a string');
+    if (typeof value !== 'string') throw Error('Name must be a string');
     this._name = value;
   }
 
@@ -17,7 +17,7 @@ export default class HolbertonCourse {
     return this._length;
   }
   set length(value) {
-    if (typeof value !== 'number') throw Error('length must be a number');
+    if (typeof value !== 'number') throw Error('Length must be a number');
     this._length = value;
   }
 
