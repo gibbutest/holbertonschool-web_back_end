09e15d023ff7afaa649d4a98bf0aa6b44a295f11
Ian <Gibbu_@hotmail.com>
2025-08-28 15:10:19 +1000

    Task 3 - init

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import { createUser, uploadPhoto } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()])
+    .then(([user, photo]) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
