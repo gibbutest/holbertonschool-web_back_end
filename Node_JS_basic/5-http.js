9f7616c3467fa84a82941e711f579d4cb6f416be
Ian <Gibbu_@hotmail.com>
2025-09-03 13:38:18 +1000

    Task 5 - whoops

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,25 +4,27 @@ const countStudents = require('./3-read_file_async.js');
 const database = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.statusCode = 200;
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     try {
-      let string = 'This is a list of our students\n';
       const { total, data } = await countStudents(database);
 
+      let string = 'This is the list of our students\n';
       string += `Number of students: ${total}\n`;
 
       Object.entries(data).forEach(([field, students]) => {
         string += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
       });
 
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.statusCode = 200;
       res.end(string);
     } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end(error);
+      res.statusCode = 500;
+      res.end(error.toString());
     }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
